a05c6e06e35277e1f90a30929c51545c575ca864
sejpalsinh <55375017+sejpalsinh@users.noreply.github.com>
2020-02-25 11:19:36 +0530

    design done

---
+++ app/src/main/java/com/example/powerautomation/bluetooth.java
@@ -0,0 +1,14 @@
+package com.example.powerautomation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+}
